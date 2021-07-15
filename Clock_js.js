975ba67b121bdbfcd07e9aff810190d9a75d3fb2
Wilibui <75092496+Wilibui@users.noreply.github.com>
2021-07-15 11:01:46 +0200

    Add files via upload

--- Clock_js.js
+++ Clock_js.js
@@ -13,10 +13,15 @@ let h;
 let m;
 let s;
 
+let u = 50;
+let numbers = [];
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  strokeWeight(height / 40);
+  createCanvas(windowWidth, windowHeight);  
   noFill(); 
+  print(width);
+  
+  u = width/30
   
   rh = height / (4/3);
   rm = height / (40/33);
@@ -28,15 +33,25 @@ function setup() {
   a = PI / 30;
   b = PI / 12;
   start = 3 * PI / 2;
+  
+  numbers[0] = new Number(2*width/40, height/6);
+  numbers[1] = new Number(5*width/40, height/6);
+  numbers[2] = new Number(9*width/40, height/6);
+  numbers[3] = new Number(12*width/40, height/6);
 }
 
 function draw() {
-  translate(width / 2, height / 2);
   background(0);
-  
   s = second();
   m = minute() + (s / 60);
   h = hour() + (m / 60);
+  digital();
+  clock();  
+}
+
+function clock(){
+  translate(2*width / 3, height / 2);
+  strokeWeight(height / 40);
     
   //lines
   stroke(255);
@@ -49,7 +64,7 @@ function draw() {
     line(x1, y1, x2, y2);
   }
         
-  // hours
+  //hours
   angle = b * h * 2 + start;
   stroke(150, 0, 0);
   line(0, 0, lh*cos(angle), lh*sin(angle));
@@ -67,4 +82,95 @@ function draw() {
   line(0, 0, ls*cos(angle), ls*sin(angle));
   arc(0, 0, rs, rs, start, angle);
   point(0, 0);
+}
+
+function digital(){
+  //hours
+  if(hour() < 10){
+    numbers[0].update(10);
+  }else{
+    numbers[0].update(floor(hour()/10));
+  }
+  numbers[1].update(hour() - floor(hour()/10)*10);  
+  
+  //minutes
+  if(minute() < 10){
+    numbers[2].update(0);
+  }else{
+    numbers[2].update(floor(minute()/10));
+  }
+  numbers[3].update(minute() - floor(minute()/10)*10);  
+  for(let number of numbers){
+    number.show();
+  }
+  
+  //dots
+  stroke(255);
+  point(7*width/40, height/6 - 0.8*u);
+  point(7*width/40, height/6 + 0.8*u);
+}
+
+class Number{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.n = 8;
+  }
+  
+  show(){
+    strokeWeight(u/3);
+    
+    //1
+    if(this.n == 0||this.n == 2||this.n == 3||this.n == 5||this.n == 6||this.n == 7||this.n == 8||this.n == 9){
+      stroke(255);
+    }else{
+      noStroke();
+    }
+    line(this.x - (0.6*u), this.y - (2*u), this.x + (0.6*u), this.y - (2*u));
+    //2
+    if(this.n == 0||this.n == 4||this.n == 5||this.n == 6||this.n == 8||this.n == 9){
+      stroke(255);
+    }else{
+      noStroke();
+    }
+    line(this.x - (0.8*u), this.y - (1.6*u), this.x - (0.8*u), this.y - (0.4*u));
+    //3
+    if(this.n == 0||this.n == 1||this.n == 2||this.n == 3||this.n == 4||this.n == 7||this.n == 8||this.n == 9){
+      stroke(255);
+    }else{
+      noStroke();
+    }
+    line(this.x + (0.8*u), this.y - (1.6*u), this.x + (0.8*u), this.y - (0.4*u));
+    //4
+    if(this.n == 2||this.n == 3||this.n == 4||this.n == 5||this.n == 6||this.n == 8||this.n == 9){
+      stroke(255);
+    }else{
+      noStroke();
+    }
+    line(this.x - (0.6*u), this.y, this.x + (0.6*u), this.y);
+    //5
+    if(this.n == 0||this.n == 2||this.n == 6||this.n == 8){
+      stroke(255);
+    }else{
+      noStroke();
+    }
+    line(this.x - (0.8*u), this.y + (0.4*u), this.x - (0.8*u), this.y + (1.6*u));
+    //6
+    if(this.n == 0||this.n == 1 ||this.n == 3||this.n == 4||this.n == 5||this.n == 6||this.n == 7||this.n == 8||this.n == 9){
+      stroke(255);
+    }else{
+      noStroke();
+    }
+    line(this.x + (0.8*u), this.y + (0.4*u), this.x + (0.8*u), this.y + (1.6*u));
+    //7
+    if(this.n == 0||this.n == 2||this.n == 3||this.n == 5||this.n == 6||this.n == 8||this.n == 9){
+      stroke(255);
+    }else{
+      noStroke();
+    }
+    line(this.x - (0.6*u), this.y + (2*u), this.x + (0.6*u), this.y + (2*u));
+  }
+  update(n){
+    this.n = n;
+  }
 }
